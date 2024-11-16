cebc12bb1433c3f8f90d005e14b1e473faf44bb9
jenanF <108689302+jenanF@users.noreply.github.com>
2024-11-17 01:49:27 +0300

    dd

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -2,6 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import wood from './images/wood.png';
 import house from './images/cabin.png';
+import React, { useState } from 'react';
+
+let woods = 0;
+let houses = 0;
+let toolsUpgrade = "upgrade";
+let showButton = false;
+
+function Increasing() {
+
+  useState(); {
+    woods++;
+    console.log(woods);
+
+    for (let i = 5; i >= 5;) {
+      if (woods % 5) {
+        houses++;
+      } else {
+        return;
+      }
+    }
+  }
+}
+
+function tools() {
+
+  if (houses >= 5) {
+    showButton = true;
+    toolsUpgrade = "upgrade to iron axe x2 faster";
+
+  } else if (houses >= 20) {
+    showButton = true;
+    toolsUpgrade = "upgrade to golden axe x5 faster";
+
+  } else if (houses >= 50) {
+    showButton = true;
+    toolsUpgrade = "upgrade to dimond axe x12 faster";
+
+  } else {
+    return;
+  }
+
+  showButton = !showButton;
+}
+
+
 
 function App() {
   return (
@@ -19,20 +64,23 @@ function App() {
 
           <div style={{ display: 'flex', flexDirection: 'row', alignContent: 'center', justifyContent: 'center' }}>
             <img src={wood} width={100} height={100}></img>
-            <h2>0</h2>
+            <h2>{woods}</h2>
           </div>
 
 
           <div style={{ display: 'flex', flexDirection: 'row', alignContent: 'center', justifyContent: 'center', marginLeft: 25 }}>
             <img src={house} width={80} height={80}></img>
-            <h2>0</h2>
+            <h2>{houses}</h2>
           </div>
 
         </div>
 
       </div>
 
-      <button className='axebutton'></button>
+      <button className='axebutton' //onClick={Increasing()}
+      ></button>
+
+      <button className='upgradebutton' title={toolsUpgrade}>{toolsUpgrade}</button>
 
     </div>
   );
